edc9d12dc71eaaacabee1f84671647004181157b
Krystal <krystalkoh@gmail.com>
2022-06-05 12:02:11 +0800

    inserting enemy

--- script.js
+++ script.js
@@ -2,62 +2,88 @@ const canvas = document.querySelector("canvas");
 const context = canvas.getContext("2d");
 
 //creating the canvas
-canvas.width = 824;
-canvas.height = 500;
-
-// context.fillStyle = "white";
-// context.fillRect(0, 0, canvas.width, canvas.height);
-
-//making collisions array --> how to make collisions?!
+canvas.width = 1024;
+canvas.height = 576;
+////////colliding zone///////////
+// creating collisions array
 // const collisionsMap = [];
 // for (let i = 0; i < collisions.length; i += 70) {
 //   collisionsMap.push(collisions.slice(i, 70 + i));
 // }
+// console.log(collisionsMap);
 
+/////////drawing boundaries/////////
 // class Boundary {
 //   static width = 48;
 //   static height = 48;
-//   constructor({ position, width, height }) {
+//   constructor({ position }) {
 //     (this.position = position), (this.width = 48), (this.height = 48);
 //   }
 
 //   draw() {
-//     context.fillStyle = "red";
-//     context.fillRect(
-//       this.position.x,
-//       this.position.y,
-//       this.position.width,
-//       this.position.height
-//     );
+//     context.fillStyle = "rgba(255,0,0,0.5)";
+//     context.fillRect(this.position.x, this.position.y, 48, 48);
 //   }
 // }
 
 // const boundaries = [];
-
+const offset = {
+  x: -1000,
+  y: -650,
+};
+////////colliding zone///////////
+// //looping over each row, i=index of the subarray
 // collisionsMap.forEach((row, i) => {
+//   //within each row, j=index that is looping over each symbol [0,1,0]
 //   row.forEach((symbol, j) => {
+//     //so that only pushing in boundaries that i want 1025
 //     if (symbol === 1025)
 //       boundaries.push(
 //         new Boundary({
-//           position: { x: j * Boundary.width, y: i * Boundary.height },
+//           position: {
+//             x: j * Boundary.width + offset.x,
+//             y: i * Boundary.height + offset.y,
+//           },
 //         })
 //       );
 //   });
 // });
-//
 
+// console.log(boundaries);
+
+// const battleZonesMap = [];
+// for (let i = 0; i < battleZoneArr.length; i += 70) {
+//   battleZonesMap.push(battleZoneArr.slice(i, 70 + i));
+// }
+
+////////battlezone///////////
+// const battleZones = [];
+// battleZonesMap.forEach((row, i) => {
+//   row.forEach((symbol, j) => {
+//     if (symbol === 1025)
+//       battleZones.push(
+//         new Boundary({
+//           position: {
+//             x: j * Boundary.width + offset.x,
+//             y: i * Boundary.height + offset.y,
+//           },
+//         })
+//       );
+//   });
+// });
+// console.log(battleZones);
 //drawing image in canvas
-const backgroundImg = document.getElementById("background");
-// backgroundImg.src = "./assets/img/backgroundMap.png";
+// const backgroundImg = document.querySelector(".background");
+const backgroundImg = document.createElement("img");
+backgroundImg.src = "./assets/img/backgroundMap.png";
 // context.drawImage(backgroundImg, -1100, -630);
-const hermione = document.getElementById("hermioneDown");
-const mandrake = document.getElementById("mandrake");
-// const hermione = document.createElement("img");
-// hermione.src = "./assets/img/hermioneDown2.png";
+// const hermione = document.getElementById("hermioneDown");
+const hermione = document.createElement("img");
+hermione.src = "./assets/img/singleHermione.png";
 
 //drawing hermione sprite in canvas
 class SpriteMoving {
-  constructor(image, position) {
+  constructor({ image, position }) {
     this.image = image;
     this.position = position;
   }
@@ -66,11 +92,31 @@ class SpriteMoving {
   }
 }
 
+class Enemy {
+  constructor({ image, position }) {
+    (this.image = image), (this.position = position);
+  }
+  draw() {
+    context.drawImage(this.image, this.position.x, this.position.y);
+  }
+}
+const mandrakeImg = document.createElement("img");
+mandrakeImg.src = "./assets/img/mandrake.png";
+
+const mandrake = new Enemy({
+  image: mandrakeImg,
+  position: { x: 400, y: 300 },
+});
+
+const player = new SpriteMoving({
+  position: { x: 444, y: 250 },
+  image: hermione,
+});
 //creating background moving class
-// const background = new SpriteMoving(backgroundImg, { x: -1100, y: -630 });
-const background = new SpriteMoving(backgroundImg, { x: 0, y: 0 });
-const hermioneMoving = new SpriteMoving(hermione, { x: 296, y: 180 });
-const mandrakeAppears = new SpriteMoving(mandrake, { x: 80, y: 50 });
+const background = new SpriteMoving({
+  image: backgroundImg,
+  position: { x: offset.x, y: offset.y },
+});
 //adding keys object --> default = false
 const keys = {
   ArrowUp: { pressed: false },
@@ -78,52 +124,36 @@ const keys = {
   ArrowLeft: { pressed: false },
   ArrowRight: { pressed: false },
 };
+////// insert ...battleZones below////
+const movables = [background, mandrake];
 
-// const mandrake = document.createElement("img");
-// mandrake.src = "./assets/img/mandrake.png";
-
-// const mandrakeClass = new SpriteMoving(mandrake, { x: 200, y: 80 });
-
-// function beast() {
-//   window.requestAnimationFrame(beast);
-//   mandrake.draw();
-// }
-
-//add animation loop
+//creating movables array
 function animate() {
   //adding infinite loop so character can move
   window.requestAnimationFrame(animate);
   background.draw();
-  mandrakeAppears.draw();
-  hermioneMoving.draw();
-  //   context.drawImage(mandrake, 200, 60);
-  //   context.drawImage(
-  //     //drawing hermione
-  //     hermione,
-  //     //cropping hermione
-  //     176,
-  //     0,
-  //     hermione.width / 4.6,
-  //     hermione.height,
-  //     //posiiton of hermione
-  //     200,
-  //     0,
-  //     hermione.width / 4,
-  //     hermione.height
-  //   );
-  //What happens when keys are pressed
+  //   battleZones.forEach((battleZone) => {
+  //     battleZone.draw();
+  //   });
+  player.draw();
+  mandrake.draw();
+
   if (keys.ArrowUp.pressed) {
-    hermioneMoving.position.y = hermioneMoving.position.y - 1;
-    // mandrakeClass.position.y = background.position.y - 1;
+    movables.forEach((movable) => {
+      movable.position.y += 3;
+    });
   } else if (keys.ArrowDown.pressed) {
-    hermioneMoving.position.y = hermioneMoving.position.y + 1;
-    // mandrakeClass.position.y = background.position.y + 1;
+    movables.forEach((movable) => {
+      movable.position.y -= 3;
+    });
   } else if (keys.ArrowLeft.pressed) {
-    hermioneMoving.position.x = hermioneMoving.position.x - 1;
-    // mandrakeClass.position.x = background.position.x - 1;
+    movables.forEach((movable) => {
+      movable.position.x += 3;
+    });
   } else if (keys.ArrowRight.pressed) {
-    hermioneMoving.position.x = hermioneMoving.position.x + 1;
-    // mandrakeClass.position.x = background.position.x + 1;
+    movables.forEach((movable) => {
+      movable.position.x -= 3;
+    });
   }
 }
 
@@ -157,3 +187,7 @@ window.addEventListener("keyup", (e) => {
 //
 
 //battle
+// const testing = document.getElementById("mandrake");
+// testing.addEventListener("click", function (e) {
+//   console.log(e.target, "mandrake works");
+// });
